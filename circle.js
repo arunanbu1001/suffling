3a699cbea48f2f53c2d7f8c9601d878bb10f72cf
arunanbu1001 <arunanbu10012001@gmail.com>
2022-07-28 21:04:28 +0530

    add

--- circle.js
+++ circle.js
@@ -1,13 +1,54 @@
 function load(){
-    let b=document.querySelectorAll('#arun');
-    let x="Arun";
+    let b=document.querySelectorAll('#niuk');
+    let x="NIGK";
     let y=x.split('');
     console.log(y);
+    var len=0
+    var d=document.querySelector(".num")
+    var sub=document.querySelector("input")
+    var balance=document.querySelector("span")
+    var ba=document.querySelector("body")
+    console.log(sub)
+    var emp=["KING","INK","IN","NGK","NI","NG"]
+    emp.length--
+    var checking=[]
     b.forEach((a)=>{
         let ran=Math.floor(Math.random()*y.length);
         a.innerHTML=y[ran];
         y.splice(ran,1);
-        console.log(ran);
+        
+        a.addEventListener("click",vl)
+        function vl(){
+          var s=this.innerHTML;
+           d.append(s)
+          console.log(d.innerHTML);
+          
+        }
        
     })
-}
+    sub.addEventListener("click",add)
+    function add(){
+      var c=emp.includes(d.innerHTML)
+      if(emp.length==1){
+        ba.innerHTML="comple"
+        console.log("hi")
+      }
+     // console.log(emp.includes(d.innerHTML))
+      if(c==true){
+         
+        
+        console.log(emp.length)
+        
+        console.log(checking)
+       var con= checking.includes(d.innerHTML)
+       
+  if(con==false){
+    checking.push(d.innerHTML)
+    console.log(con)
+        balance.innerHTML= emp.length--
+  }
+      }
+      d.innerHTML='';
+    }
+    
+  }
